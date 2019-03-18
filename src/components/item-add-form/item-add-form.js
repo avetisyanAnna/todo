c250994954c00c2e980baba731c3a598b8b01537
avetisyanAnna <annamariavetis@gmail.com>
2019-03-18 22:03:34 +0400

    add item flow, minor refactoring

---
+++ src/components/item-add-form/item-add-form.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './item-add-form.css';
+
+class ItemAddForm extends Component {
+    render() {
+        return (
+            <div className="item-add-form">
+                <button
+                    className="btn btn-outline-secondary"
+                onClick={() => this.props.onItemAdded('sdfsdfsd')}>Add Item</button>
+            </div>
+        );
+    }
+}
+
+export default ItemAddForm;
--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-
-import './item-status-filter.css';
+import React, {Component} from 'react';
 
 export default class ItemStatusFilter extends Component {
     render() {
         return (
             <div className="btn-group">
                 <button type="button"
-                        className="btn btn-info">All</button>
+                        className="btn btn-info">All
+                </button>
                 <button type="button"
-                        className="btn btn-outline-secondary">Active</button>
+                        className="btn btn-outline-secondary">Active
+                </button>
                 <button type="button"
-                        className="btn btn-outline-secondary">Done</button>
+                        className="btn btn-outline-secondary">Done
+                </button>
             </div>
         );
 
