d6adbc5958cec052ce37b2520504e0c050649c09
Anna Avetisyan <anna.avetisyan@iunetworks.am>
2019-03-19 17:34:56 +0400

    added input(add item flow)

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import './item-add-form.css';
 
 class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (event) => {
+        this.setState({
+            label: event.target.value
+        });
+    };
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
     render() {
         return (
-            <div className="item-add-form">
-                <button
-                    className="btn btn-outline-secondary"
-                onClick={() => this.props.onItemAdded('sdfsdfsd')}>Add Item</button>
-            </div>
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text" className="form-control g-flex" onChange={this.onLabelChange}
+                       placeholder="What needs to be done" value={this.state.label}/>
+                <button className="btn btn-outline-secondary">Add Item</button>
+            </form>
         );
     }
 }
